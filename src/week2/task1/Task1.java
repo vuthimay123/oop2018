68baafabc824b68aa3998b0578769a1ea815b8d7
= <=>
2018-09-25 11:33:26 +0700

    add'

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,14 +1,51 @@
 package week2.task1;
 
+import java.util.Scanner;
+
 public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        while(b!=0){
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+
+        return a;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+       // int fibonacci1=0;
+        int a=0;
+        int b=1;
+        int fibonacci1=a+b;
+        while(b<n-2) {
+
+            a=b;
+            b=fibonacci1;
+            fibonacci1 = a + b;
+        }
+        return fibonacci1;
+    }
+    public static void main(String[] args)
+    {
+        int a,b;
+        Scanner input=new Scanner(System.in);
+        System.out.println("nhap so thu nhat:");
+        a=input.nextInt();
+        System.out.println("nhap so thu hai ");
+        b=input.nextInt();
+        int ucln=gcd(a,b);
+        System.out.println(ucln);
+        System.out.println("ket qua cua day fibonaccil");
+        int n=input.nextInt();
+
+        System.out.println(fibonacci(n));
+
+
     }
 }
+
+
--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,30 +1,59 @@
 package week2.task2;
 
+import sun.net.ftp.FtpClient;
+
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
+    private int numerator, denominator;
+
 
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
     public Fraction add(Fraction other) {
-        // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        // TODO: Ph&#x1b0;&#x1a1;ng th&#x1ee9;c c&#x1ed9;ng hai ph&acirc;n s&#x1ed1; (this v&agrave; other), tr&#x1ea3; v&#x1ec1; &#x111;&#x1ed1;i t&#x1b0;&#x1ee3;ng Fraction m&#x1edb;i
+        Fraction sum1 = new Fraction(0, 0);
+        sum1.numerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        sum1.denominator = this.denominator * other.denominator;
+
+
+        return sum1;
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction hieu = new Fraction(0, 0);
+
+        hieu.numerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        hieu.denominator = this.denominator * other.denominator;
+
+        return hieu;
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction tich = new Fraction(0, 0);
+        tich.numerator = this.numerator * other.numerator;
+        tich.denominator = this.denominator * other.denominator;
+
+        return tich;
     }
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        return null;
+        Fraction thuong = new Fraction(0, 0);
+        thuong.numerator = this.numerator * other.denominator;
+        thuong.denominator = this.denominator * other.numerator;
+
+        return thuong;
+    }
+
+    public boolean equals(Fraction other) {
+        return (this.numerator * other.denominator) == (this.denominator * other.numerator);
     }
 }
+//may
